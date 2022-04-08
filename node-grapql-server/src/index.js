5d8d73a8c95f023c20e514959c3c23cbb26f8c0e
Atabek Kurbanov <kurbanof.atabek@gmail.com>
2022-04-09 05:02:33 +0600

    created simple GraphQL types and resolvers

--- node-grapql-server/src/index.js
+++ node-grapql-server/src/index.js
@@ -1,9 +1,67 @@
 // index.js
 // This is the main entry point of our application
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+let notes = [
+  {
+    id: '1',
+    content: 'This is a note',
+    author: 'Adam Scott'
+  },
+  {
+    id: '2',
+    content: 'This is another note',
+    author: 'Harlow Everly'
+  },
+  {
+    id: '3',
+    content: 'Oh hey look, another note!',
+    author: 'Riley Harrison'
+  }
+];
+
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => notes.find(note => note.id === args.id)
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      const newNote = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Yuzeyzer'
+      };
+      notes.push(newNote);
+      return newNote;
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+server.applyMiddleware({ app, path: '/api' });
 
 app.get('/', (req, res) => res.send('Hello World!!!'));
 
