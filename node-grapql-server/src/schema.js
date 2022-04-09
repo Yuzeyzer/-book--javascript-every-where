d47c8f02633d6b0083b50a40a220009c87e0ed4e
Atabek Kurbanov <kurbanof.atabek@gmail.com>
2022-04-10 04:40:12 +0600

    created user model and User type

--- node-grapql-server/src/schema.js
+++ node-grapql-server/src/schema.js
@@ -2,10 +2,17 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
   scalar DateTime
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String!
+    notes: [Note!]!
+  }
   type Note {
     id: ID!
     content: String!
-    author: String!
+    author: User!
     createdAt: DateTime!
     updatedAt: DateTime!
   }
@@ -15,8 +22,10 @@ module.exports = gql`
     note(id: ID!): Note!
   }
   type Mutation {
-    newNote(content: String!): Note!,
+    newNote(content: String!): Note!
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String, email: String, password: String!): String!
   }
 `;
